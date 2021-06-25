31eb399ee27b0072d4383d19bf26a13443f76958
peter greaves <peter.greaves64@gmail.com>
2021-06-25 16:24:11 +0100

    added csrf to create and update beer and added IT for them

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -1,11 +1,16 @@
 package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.web.servlet.function.RequestPredicates.param;
 
 @SpringBootTest
 public class BeerControllerIT extends BaseIT{
@@ -46,6 +51,28 @@ public class BeerControllerIT extends BaseIT{
                 .andExpect(status().isUnauthorized());
     }
 
+    @Test
+    void addBeersUNAUTH() throws Exception {
+        mockMvc.perform(post("/beers/new").with(csrf())
+                .param("beerName", "old smokey"))
+                .andExpect(status().isUnauthorized());
+    }
 
+
+    @ParameterizedTest(name = "#{index} with [{arguments}]")
+    @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamNotAdmin")
+    void addBeerAsNOTADMIN(String username, String password) throws Exception {
+        mockMvc.perform(post("/beers/new",param("beerName", "Norse mullah"))
+                .with(httpBasic(username, password)))
+                .andExpect(status().isForbidden());
+    }
+
+    @ParameterizedTest(name = "#{index} with [{arguments}]")
+    @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamAdminOnly")
+    void addBeerAsADMIN(String username, String password) throws Exception {
+        mockMvc.perform(post("/beers/new").with(csrf())
+                .with(httpBasic(username, password)))
+                .andExpect(status().is3xxRedirection());
+    }
 
 }
